00a3ca2eb5bedddeab134d2e238ed8404b86e3c6
tammy-baylis-swi <tammy.baylis@solarwinds.com>
2024-04-30 13:33:45 -0700

    Diff from 2101

--- opentelemetry-instrumentation/src/opentelemetry/instrumentation/bootstrap_gen.py
+++ opentelemetry-instrumentation/src/opentelemetry/instrumentation/bootstrap_gen.py
@@ -25,10 +25,6 @@ libraries = [
         "instrumentation": "opentelemetry-instrumentation-aiohttp-client==0.46b0.dev",
     },
     {
-        "library": "aiohttp ~= 3.0",
-        "instrumentation": "opentelemetry-instrumentation-aiohttp-server==0.46b0.dev",
-    },
-    {
         "library": "aiopg >= 0.13.0, < 2.0.0",
         "instrumentation": "opentelemetry-instrumentation-aiopg==0.46b0.dev",
     },
